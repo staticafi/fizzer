eb9f77d9696a92a2a3133f94465e08763949d8e1
Marek Trtik <trtikm@gmail.com>
2025-04-17 23:11:43 +0200

    Removed obsolete comment

--- src/connection/include/connection/target_executor.hpp
+++ src/connection/include/connection/target_executor.hpp
@@ -36,39 +36,3 @@ private:
 }
 
 #endif
-
-
-
-
-    // void init_shared_memory(std::size_t size);
-    // void set_timeout(natural_16_bit timeout_ms_);
-    // void execute_target();
-
-
-// #ifndef CONNECTION_BENCHMARK_EXECUTOR_HPP_INCLUDED
-// #   define CONNECTION_BENCHMARK_EXECUTOR_HPP_INCLUDED
-
-// #   include <connection/target_executor.hpp>
-// #   include <utility/basic_numeric_types.hpp>
-// #   include <memory>
-// #   include <string>
-
-
-// namespace connection {
-
-
-// struct  benchmark_executor_via_shared_memory
-// {
-//     benchmark_executor_via_shared_memory(std::string const&  path_to_target);
-//     ~benchmark_executor_via_shared_memory();
-//     void  operator()();
-//     void  on_io_config_changed();
-
-// private:
-//     std::unique_ptr<target_executor>  executor;
-// };
-
-
-// }
-
-// #endif
