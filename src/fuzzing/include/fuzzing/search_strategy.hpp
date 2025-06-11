23918c37254d0e72b90d9240c443d2c51ce9d625
Marek Trtik <trtikm@gmail.com>
2025-06-11 20:15:46 +0200

    Strategy refactoring: moving enums and cursor into cpp file

--- src/fuzzing/include/fuzzing/search_strategy.hpp
+++ src/fuzzing/include/fuzzing/search_strategy.hpp
@@ -6,30 +6,13 @@
 #   include <map>
 #   include <deque>
 
+namespace  fuzzing { struct  navigation_cursor; }
+
 namespace  fuzzing {
 
 
 struct search_strategy
 {
-    enum METRIC_TYPE
-    {
-        MT_BEST_VALUE  = 0U,
-        MT_INPUT_SIZE  = 1U,
-        MT_HIT_COUNT   = 2U,
-        NUM_METRIC_TYPES
-    };
-
-    enum FILTER_TYPE
-    {
-        FT_ALL         = 0U,
-        FT_WARM        = 1U,
-        FT_COLD        = 2U,
-        FT_INPUT_USE   = 3U,
-        FT_INPUT_WARM  = 4U,
-        FT_INPUT_COLD  = 5U,
-        NUM_FILTER_TYPES
-    };
-
     using  location_props = std::deque<branching_node*>;
     using  locations_map = std::map<location_id, location_props>;
 
@@ -46,24 +29,8 @@ struct search_strategy
 
 private:
 
-    struct  navigation_cursor
-    {
-        navigation_cursor(locations_map*  locations_);
-        void  next();
-        void  on_insert_location(location_id  id);
-        void  on_erase_location(location_id  id);
-        bool  operator==(navigation_cursor const&  other) const { return  location == other.location && metric == other.metric && filter == other.filter; }
-        bool  operator!=(navigation_cursor const&  other) const { return  !(*this == other); }
-        bool  valid() const { return location != locations->end(); }
-        locations_map::iterator  location;
-        METRIC_TYPE  metric;
-        FILTER_TYPE  filter;
-    private:
-        locations_map*  locations;
-    };
-
     locations_map  locations;
-    navigation_cursor  cursor;
+    navigation_cursor*  cursor;
 
     natural_16_bit  MAX_NODES;
 };
