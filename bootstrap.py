d33e47af9a8f1342cc554d46bdd44e957297ded2
Marek Trtik <trtikm@gmail.com>
2025-08-08 15:52:08 +0200

    Added CPS library to the project

--- bootstrap.py
+++ bootstrap.py
@@ -8,3 +8,4 @@ subprocess.run(["git", "-c", "protocol.file.allow=always", "submodule", "add", "
 subprocess.run(["git", "-c", "protocol.file.allow=always", "submodule", "add", "https://github.com/staticafi/binsalac.git", "./src/tools/salac"])
 subprocess.run(["git", "-c", "protocol.file.allow=always", "submodule", "add", "https://github.com/staticafi/binsalat.git", "./src/tools/salat"])
 subprocess.run(["git", "-c", "protocol.file.allow=always", "submodule", "add", "https://github.com/staticafi/datsalat.git", "./benchmarks/salatd"])
+subprocess.run(["git", "-c", "protocol.file.allow=always", "submodule", "add", "https://github.com/staticafi/libcps.git", "./src/cps"])
