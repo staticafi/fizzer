725320e6d64ded4a28860454b1d3dba609d056d4
Marek Trtik <trtikm@gmail.com>
2025-09-15 18:19:59 +0200

    Construction of the execution tree from tests.

--- src/tools/cpseval/run.cpp
+++ src/tools/cpseval/run.cpp
@@ -2,6 +2,7 @@
 #include <cpseval/program_options.hpp>
 #include <cpseval/load_tests.hpp>
 #include <fuzzing/target_executor.hpp>
+#include <fuzzing/branching_node.hpp>
 #include <sala/program.hpp>
 #include <sala/streaming.hpp>
 #include <com/mut_type.hpp>
@@ -112,12 +113,118 @@ void run(int argc, char* argv[])
                 )
             };
 
-    std::vector<test_case_ptr>  tests;
-    if (!load_tests(get_program_options()->value("path_to_tests"), get_program_options()->value("source_file_name"), tests))
+    fuzzing::branching_node*  entry_branching{ nullptr };
+    std::unordered_set<fuzzing::branching_node*>  leaf_branchings{};
     {
-        std::cerr << "ERROR: Cannot read the test-suite for program '" << get_program_options()->value("source_file_name")
-                  << "' from ZIP file '" << get_program_options()->value("path_to_tests") << "'.\n";
-        return;
+        std::vector<test_case_ptr>  tests;
+        if (!load_tests(get_program_options()->value("path_to_tests"), get_program_options()->value("source_file_name"), tests))
+        {
+            std::cerr << "ERROR: Cannot read the test-suite for program '" << get_program_options()->value("source_file_name")
+                    << "' from ZIP file '" << get_program_options()->value("path_to_tests") << "'.\n";
+            return;
+        }
+        for (test_case_ptr test : tests)
+        {
+            fuzzing::execution_results_ptr const  results{ target_executor.run(test->bytes, test->types, test->metadata) };
+            if (!results->get_trace()->empty())
+            {
+                fuzzing::typed_input_ptr const  current_input{
+                        std::make_shared<fuzzing::typed_input>(results->get_bytes(), results->get_types(), results->get_metadata())
+                        };
+                fuzzing::execution_trace_ptr const  trace = results->get_trace();
+                fuzzing::branching_node*  leaf{ nullptr };
+                bool  diverged{ false };
+
+                if (entry_branching == nullptr)
+                {
+                    entry_branching = new fuzzing::branching_node(
+                            trace->front().id,
+                            0,
+                            trace->front().num_input_bytes,
+                            trace->front().xor_like_branching_function,
+                            trace->front().predicate,
+                            nullptr,
+                            current_input,
+                            trace,
+                            1U
+                            );
+                    diverged = true;
+                }
+
+                leaf = entry_branching;
+
+                fuzzing::trace_index_type  trace_index = 0;
+                for (; true; ++trace_index)
+                {
+                    fuzzing::trace_item const&  info = trace->at(trace_index);
+
+                    INVARIANT(leaf->get_location_id() == info.id);
+
+                    // Here we try to remove bad float (INF, NaN) from 'info.value'.
+                    if (!std::isfinite(info.value) || std::isnan(info.value))
+                    {
+                        fuzzing::branching_value&  value_ref{ const_cast<fuzzing::branching_value&>(info.value) };
+                        switch (info.predicate)
+                        {
+                            case fuzzing::atomic_predicate::EQUAL:
+                                value_ref = info.direction ? 0.0 : std::numeric_limits<fuzzing::branching_value>::max();
+                                break;
+                            case fuzzing::atomic_predicate::UNEQUAL:
+                                value_ref = info.direction ? std::numeric_limits<fuzzing::branching_value>::max() : 0.0;
+                                break;
+                            case fuzzing::atomic_predicate::LESS_EQUAL:
+                            case fuzzing::atomic_predicate::LESS:
+                                value_ref = (info.direction ? -1.0 : 1.0) * std::numeric_limits<fuzzing::branching_value>::max();
+                                break;
+                                break;
+                            case fuzzing::atomic_predicate::GREATER:
+                            case fuzzing::atomic_predicate::GREATER_EQUAL:
+                                value_ref = (info.direction ? 1.0 : -1.0) * std::numeric_limits<fuzzing::branching_value>::max();
+                                break;
+                            default: UNREACHABLE(); break;
+                        }
+                    }
+
+                    if (trace_index + 1 == trace->size())
+                        break;
+
+                    if (leaf->successor(info.direction).pointer == nullptr)
+                    {
+                        fuzzing::trace_item const&  succ_info = trace->at(trace_index + 1);
+                        leaf->set_successor(info.direction, {
+                            fuzzing::branching_node::successor_pointer::VISITED,
+                            new fuzzing::branching_node(
+                                succ_info.id,
+                                trace_index + 1,
+                                succ_info.num_input_bytes,
+                                succ_info.xor_like_branching_function,
+                                succ_info.predicate,
+                                leaf,
+                                current_input,
+                                trace,
+                                1U
+                                )
+                        });
+                        diverged = true;
+                    }
+
+                    leaf = leaf->successor(info.direction).pointer;
+                }
+
+                leaf->set_successor(trace->back().direction, {
+                    std::max(
+                        results->get_termination() == fuzzing::target_termination::NORMAL ?
+                            fuzzing::branching_node::successor_pointer::END_NORMAL :
+                            fuzzing::branching_node::successor_pointer::END_EXCEPTIONAL,
+                        leaf->successor(trace->back().direction).label
+                        ),
+                    leaf->successor(trace->back().direction).pointer
+                });
+
+                if (diverged)
+                    leaf_branchings.insert(leaf);
+            }
+        }
     }
 
     // TODO!
