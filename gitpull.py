a38416bffa962fa36b7eb27c18c8e83a5ffbbf5c
Marek Trtik <trtikm@gmail.com>
2026-05-20 17:00:44 +0200

    Updated pull script to prune deleted remote branches.

--- gitpull.py
+++ gitpull.py
@@ -3,3 +3,4 @@ import subprocess
 subprocess.run(["git", "submodule", "update", "--init", "--recursive", "--remote", "--rebase"])
 subprocess.run(["git", "submodule", "foreach", "--recursive", "git", "checkout", "main"])
 subprocess.run(["git", "pull", "--all"])
+subprocess.run(["git", "fetch", "--prune"])
