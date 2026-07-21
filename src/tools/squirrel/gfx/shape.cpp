9cca2c2c77aefb889fd040bdf2cd2d69ba591482
Marek Trtik <trtikm@gmail.com>
2026-07-21 18:40:24 +0200

    Added support for drawing arrows.

--- src/tools/squirrel/gfx/shape.cpp
+++ src/tools/squirrel/gfx/shape.cpp
@@ -3,11 +3,8 @@
 namespace gfx {
 
 
-void make_arrow_lines(std::vector<vec2>& lines, vec2 const& a, vec2 const& b)
+void make_arrow_end_lines(std::vector<vec2>& lines, vec2 const& a, vec2 const& b)
 {
-    lines.push_back(a);
-    lines.push_back(b);
-
     vec2 const x{ normalized(a - b) };
     vec2 const y{ orthogonal_ccw(x) };
 
@@ -22,10 +19,29 @@ void make_arrow_lines(std::vector<vec2>& lines, vec2 const& a, vec2 const& b)
 }
 
 
+void make_arrow_lines(std::vector<vec2>& lines, vec2 const& a, vec2 const& b)
+{
+    lines.push_back(a);
+    lines.push_back(b);
+
+    make_arrow_end_lines(lines, a, b);
+}
+
+
 void draw_arrow(ImDrawList& dl, vec2 const& a, vec2 const& b, ImColor color, float thickness)
 {
     std::vector<vec2> lines;
     make_arrow_lines(lines, a, b);
+    for (std::size_t i = 0ULL; i < lines.size(); i += 2ULL)
+        dl.AddLine(lines.at(i), lines.at(i+1ULL), color, thickness);
+}
+
+
+void draw_arrow_curved(ImDrawList& dl, vec2 const& a, vec2 const& mid, vec2 const& b, ImColor color, float thickness)
+{
+    dl.AddBezierQuadratic(a, mid, b, color, thickness);
+    std::vector<vec2> lines;
+    make_arrow_end_lines(lines, mid, b);
     for (std::size_t i = 0ULL; i < lines.size(); i += 2ULL)
         dl.AddLine(lines.at(i), lines.at(i+1ULL), color, thickness);
 }
--- src/tools/squirrel/gfx/shape.hpp
+++ src/tools/squirrel/gfx/shape.hpp
@@ -3,14 +3,19 @@
 
 #   include <squirrel/gfx/math.hpp>
 #   include <vector>
+#   include <utility>
 
 namespace gfx {
 
 
 void make_arrow_lines(std::vector<vec2>& lines, vec2 const& a, vec2 const& b);
+void make_arrow_end_lines(std::vector<vec2>& lines, vec2 const& a, vec2 const& b);
 
+inline std::pair<vec2, vec2> point_and_derivative_in_middle_of_curved_arrow(vec2 const& a, vec2 const& mid, vec2 const& b)
+{ return { 0.25f * a + 0.5f * mid + 0.25f * b, b - a }; }
 
 void draw_arrow(ImDrawList& dl, vec2 const& a, vec2 const& b, ImColor color, float thickness = 1.0f);
+void draw_arrow_curved(ImDrawList& dl, vec2 const& a, vec2 const& mid, vec2 const& b, ImColor color, float thickness);
 
 
 }
