aa26a822858badd20e9edf338dbfb8184263c973
Marek Trtik <trtikm@gmail.com>
2026-08-26 02:37:28 +0200

    Target executor uses boost::process::v1

--- src/connection/src/target_executor.cpp
+++ src/connection/src/target_executor.cpp
@@ -1,12 +1,13 @@
 #include <connection/target_executor.hpp>
-#include <boost/process.hpp>
+#include <boost/process/v1.hpp>
 
+namespace bp = boost::process::v1;
 
 /* boost process wait_for waits for the full duration if the process exited 
 before wait_for (https://github.com/boostorg/process/issues/69) 
 the wrapper is a workaround for this issue */
 template <typename Rep, typename Period>
-static bool wait_for_wrapper(boost::process::child& process, const std::chrono::duration<Rep, Period>& rel_time)
+static bool wait_for_wrapper(bp::child& process, const std::chrono::duration<Rep, Period>& rel_time)
 {
     if (process.running()) {
         return process.wait_for(rel_time);
@@ -32,7 +33,6 @@ process_termination  target_executor::run()
 {
     process_termination  info;
     {
-        namespace bp = boost::process;
         using namespace std::chrono_literals;
         bp::child target = bp::child(m_path_to_target, bp::std_out > bp::null, bp::std_err > bp::null);
         if (!wait_for_wrapper(target, std::chrono::milliseconds(m_max_exec_milliseconds)))
